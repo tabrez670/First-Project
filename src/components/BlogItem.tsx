217b6a306c877bd79cb4831cb7a83655648ccd72
Ayaan2907 <ayaanansari7921@outlook.com>
2023-03-28 03:09:45 +0530

    styled blog item card

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from "react";
+import { BlogParams } from "../types";
+import { useStyles } from "../styles/BlogItem.style";
+import { Card, Text, Group,  Image, Badge } from "@mantine/core";
 
-export default function BlogItem({blog}:any) {
-  return (<>
-    <div>{blog.title}</div>
-    <div>{blog.description}</div>
-  </>
-  )
+interface BlogItemProps {
+    blog: BlogParams;
+}
+
+export default function BlogItem({ blog }: BlogItemProps) {
+    const { classes, cx } = useStyles();
+
+    const linkProps = {
+        href: blog.redirectUrl,
+        target: "_blank",
+    };
+    return (
+        <>
+            <Card withBorder radius="md" className={cx(classes.card)}>
+                <Card.Section>
+                    <a {...linkProps}>
+                        <Image src={blog.imageUrl} height={180} />
+                    </a>
+                </Card.Section>
+                {blog.trending && (
+                    <Badge
+                        className={classes.trend}
+                        variant="gradient"
+                        gradient={{ from: "yellow", to: "red" }}
+                    >
+                        Trending
+                    </Badge>
+                )}
+
+                <Text
+                    className={classes.title}
+                    fw={500}
+                    component="a"
+                    {...linkProps}
+                >
+                    {blog.title}
+                </Text>
+
+                <Text fz="sm" color="dimmed" lineClamp={4}>
+                    {blog.description}
+                </Text>
+
+                <Group position="apart" className={classes.footer}>
+                    <Text fz="sm" inline>
+                        category: {blog.categorySlug}
+                    </Text>
+                </Group>
+            </Card>
+        </>
+    );
 }
